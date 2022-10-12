a74528ea41ad3b709a9b06af5cc64a0109824e68
Thomas <Thomas@Tannenberg.online>
2022-10-12 15:57:54 +0200

    Changed print method to - print line of stars (width) - print square of stars (hight and width)

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,39 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-
         int[] userInput = scan();
-        print(userInput[0],userInput[1]);
-
+        int hight = (userInput[0]);
+        int width = (userInput[1]);
+        printStars(width);
+        System.out.println("---");
+        rectancle(hight, width);
     }
-    public static void print(int printStart, int printEnd) {
-        int start = printStart;
-        int end = printEnd;
+    public static void maxHight(int hight) {
+        for (int z = 1; z <= hight; z++) {
+            System.out.println();
 
-        while (start <= end) {
-            System.out.println(" ");
-            for (int i = printStart; i <= printEnd; i++) {
-                System.out.print("*");
-            }
-            start++;
         }
-
+    }
+    public static void printStars(int width) {
+        for (int j = 0; j < width; j++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+    }
+    public static void rectancle(int hight, int width) {
+        int counter = 0;
+        while (counter < hight) {
+            printStars(width);
+            counter++;
+        }
     }
     public static int[] scan() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give a number:");
+        System.out.println("Set hight:");
         int scan1 = Integer.valueOf(scanner.nextLine());
 
-        System.out.println("Give a second number:");
+        System.out.println("set width:");
         int scan2 = Integer.valueOf(scanner.nextLine());
 
         return new int[] {scan1, scan2};
