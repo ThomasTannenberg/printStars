6893ab534d1e3a37d8284b8aae0fb643df2c34c2
Thomas <Thomas@Tannenberg.online>
2022-10-12 16:19:27 +0200

    implemented reverse triangle printing

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,24 @@ public class Main {
         int hight = (userInput[0]);
         int width = (userInput[1]);
         printStars(width);
-        System.out.println("---");
+        System.out.println("-------");
         rectancle(hight, width);
+        System.out.println("-------");
+        triangle(hight);
+        System.out.println("-------");
+        rightTriangle(hight);
     }
     public static void maxHight(int hight) {
         for (int z = 1; z <= hight; z++) {
             System.out.println();
 
         }
+    }
+    public static void printSpace(int hight) {
+        for (int i = 0; i < hight ; i++) {
+            System.out.print(" ");
+        }
+
     }
     public static void printStars(int width) {
         for (int j = 0; j < width; j++) {
@@ -25,6 +35,23 @@ public class Main {
         while (counter < hight) {
             printStars(width);
             counter++;
+        }
+    }
+    public static void triangle(int hight) {
+        int counter = 1;
+        while (counter <= hight) {
+            printStars(counter);
+            counter++;
+        }
+    }
+    public static void rightTriangle(int hight) {
+        int counter = 1;
+        int counter2 = hight;
+        while (counter <= hight) {
+            printSpace(counter2);
+            printStars(counter);
+            counter++;
+            counter2--;
         }
     }
     public static int[] scan() {
